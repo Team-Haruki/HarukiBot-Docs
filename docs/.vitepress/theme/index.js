bf86edba2b69264ae62d20dbec8ae291b0b7bfc5
Aposetles <135709150+Aposetles@users.noreply.github.com>
2026-05-01 14:18:31 +0800

    Register CollapseBox component in theme

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,7 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
+import CollapseBox from '../../bot-help/components/CollapseBox.vue'
 import './style.css'
 
 /** @type {import('vitepress').Theme} */
@@ -12,6 +13,6 @@ export default {
     })
   },
   enhanceApp({ app, router, siteData }) {
-    // ...
+    app.component('CollapseBox', CollapseBox)
   }
 }
